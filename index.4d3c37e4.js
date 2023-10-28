7b68b942d39d64c665f39825f059192c22f2e696
TimMedin <126110003+TimMedin@users.noreply.github.com>
2023-10-28 16:34:53 +0000

    Deploying to gh-pages from @ TimMedin/iceCream-team-project@b99eeb8571436c0462d9edbf9977a08d787b55c0 🚀

---
+++ index.4d3c37e4.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},scrollbar:{el:".swiper-scrollbar"},fadeEffect:{crossFade:!0},keyboard:{enabled:!0,onlyInViewport:!1}});
+//# sourceMappingURL=index.4d3c37e4.js.map
